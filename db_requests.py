1ba3dc6bd035b1598be2e8e7fb6c5d432894b822
wurst_pilotLP <jonathan.elia.kayser@icloud.com>
2026-09-22 19:06:43 +0200

    Finished the requests for editing the database

--- db_requests.py
+++ db_requests.py
@@ -1,6 +1,5 @@
 import sqlalchemy as db_lib
 import db_config as db
-from db_config import Base
 
 
 async def new_member_entry(userid):
@@ -12,14 +11,22 @@ async def new_member_entry(userid):
         await session.commit()
 
 async def change_mcname(userid, mcname):
-    async with db.SessionLocal() as session:
-        search_statement = db_lib.select(db.Member).where(db.Member.userid == userid)
+    if mcname == "":
+        return False
+    else:
+        async with db.SessionLocal() as session:
+            search_statement = db_lib.select(db.Member).where(db.Member.userid == userid)
 
-        searched_user = await session.execute(search_statement)
+            result = await session.execute(search_statement)
 
-        if searched_user == None:
-            return
+            searched_user = result.scalar_one_or_none()
 
+            if searched_user is not None:
+                searched_user.mcname = mcname
+                await session.commit()
+                return True
+            else:
+                return False
 
 async def get_mcname(userid):
     async with db.SessionLocal() as session:
@@ -30,13 +37,35 @@ async def get_mcname(userid):
         searched_user = result.scalar_one_or_none()
 
         if searched_user is not None:
-            if searched_user.mcname != None:
+            if searched_user.mcname is not None:
                 return searched_user.mcname
             else:
                 return ""
         else:
             return None
 
+async def delete_member(userid):
+    async with db.SessionLocal() as session:
+
+        search_statement = db_lib.select(db.Member).where(db.Member.userid == userid)
+        result = await session.execute(search_statement)
+        searched_user = result.scalar_one_or_none()
+
+        if searched_user is not None:
+            await session.delete(searched_user)
+            await session.commit()
+            return True
+        else:
+            return False
 
 async def get_known_players():
-    pass
+    async with db.SessionLocal() as session:
+
+        search_statement = db_lib.select(db.Member.mcname).where(db.Member.mcname is not None)
+        result = await session.execute(search_statement)
+        unpacked_names = result.scalars()
+        list_mcnames = list(unpacked_names.all())
+        if list_mcnames:
+            return list_mcnames
+        else:
+            return None
