b2bd57db1d03adf0fbfca925c0754a395a1c53b0
wurst_pilotLP <jonathan.elia.kayser@icloud.com>
2026-09-26 00:15:28 +0200

    added the embeds for the activity feed

--- embeds.py
+++ embeds.py
@@ -1,9 +1,26 @@
+from datetime import datetime, timezone
+
 import discord
 
-async def get_list_bulletpoints(values,):
+async def get_list_bulletpoints(values):
     return "/n".join(f"• {name}" for name in values)
 
+#region mc_activity_feed
 async def get_online_members_embed(online_members):
     embed = discord.Embed(
-        title="The following members"
+        title="The following members are online",
+        colour=discord.Colour.green(),
+        description = await get_list_bulletpoints(online_members),
+        timestamp = datetime.now(timezone.utc)
     )
+    return embed
+
+async def get_no_online_members_embed():
+    embed = discord.Embed(
+        title="There are no online members",
+        colour=discord.Colour.red(),
+        description = "Be the first one!",
+        timestamp = datetime.now(timezone.utc)
+    )
+    return embed
+#endregion
