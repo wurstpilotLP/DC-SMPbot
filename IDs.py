5ce21ca360fdc0dfd54158bc72bc3f7e175ed222
wurst_pilotLP <jonathan.elia.kayser@icloud.com>
2026-09-26 00:50:50 +0200

    Minor changes for my personal usercase

--- IDs.py
+++ IDs.py
@@ -1,4 +1,4 @@
 # Channel-IDs:
 welcome_channel_id=1521803326713368697    # put your own channel-id in place of the number
-service_channel_id = 0
-activity_channel_id = 0
+service_channel_id = 1553176236371476611
+activity_channel_id = 1553176283377303562
